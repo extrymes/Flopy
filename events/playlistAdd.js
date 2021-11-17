7732b5b86941aa3cccef458ff41626c6e15b617c
Extrymes <simon.abras@gmail.com>
2021-11-17 16:10:24 +0100

    Improvement

--- events/playlistAdd.js
+++ events/playlistAdd.js
@@ -1,5 +1,5 @@
 module.exports = async (client, queue, playlist) => {
     const guild = queue.guild
-    client.updateDashboard(guild)e
+    client.updateDashboard(guild)
     if(queue?.songs?.length === playlist?.songs?.length) client.sendCommands(guild)
 }
