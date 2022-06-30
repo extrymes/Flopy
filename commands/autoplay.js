6482139389d3d5229eafe2f6eaf2f2a7ff1ed64b
Extrymes <simon.abras@gmail.com>
2022-06-30 15:40:26 +0200

    Added Automatic update of the dashboard with the autoplay command and filters

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -5,6 +5,7 @@ module.exports.run = async (client, message, args, settings, queue, lang) => {
     if(!client.checkVoice(guild, member)) return client.sendError(channel, `${lang.ERROR_USER_NO_VOICE_2}`)
     if(client.cooldown("autoplay" + guild.id, 2000)) return client.sendError(channel, `${lang.ERROR_ACTION_TOO_FAST}`)
     const autoplay = client.distube.toggleAutoplay(queue)
+    client.updateDashboard(guild, queue, lang)
     client.sendMessage(channel, `${autoplay ? lang.MESSAGE_QUEUE_AUTOPLAY_ENABLED : lang.MESSAGE_QUEUE_AUTOPLAY_DISABLED}`)
 }
 module.exports.data = {
