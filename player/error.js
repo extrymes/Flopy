2246b1789c501a8598c421bdd18c34a2b13d27c6
Extrymes <simon.abras@gmail.com>
2022-07-19 16:39:10 +0200

    Changed Error message when client does not have permission to join a voice channel

--- player/error.js
+++ player/error.js
@@ -4,7 +4,7 @@ module.exports = async (client, channel, error) => {
     const settings = await client.getGuild(guild)
     const lang = require(`../util/lang/${settings.flopy1.language}`)
  
-    if(message.includes("You do not have permission to join this voice channel") || message.includes("Cannot connect to the voice channel") || message.includes("The voice channel is full")) client.sendError(channel, `${lang.ERROR_VOICE_UNABLE_JOIN}`)
+    if(message.includes("I do not have permission to join this voice channel") || message.includes("Cannot connect to the voice channel") || message.includes("The voice channel is full")) client.sendError(channel, `${lang.ERROR_VOICE_UNABLE_JOIN}`)
     else if(message.includes("No result found") || message.includes("Cannot resolve undefined to a Song") || message.includes("search string is mandatory")) client.sendError(channel, `${lang.ERROR_RESULT_NO_FOUND}`)
     else if(message.includes("Video unavailable") || message.includes("This video is unavailable")) client.sendError(channel, `${lang.ERROR_VIDEO_UNAVAILABLE}`)
     else if(message.includes("Sign in to confirm your age") || message.includes("Sorry, this content is age-restricted") || message.includes("This video is only available to Music Premium members")) client.sendError(channel, `${lang.ERROR_VIDEO_RESTRICTED}`)
