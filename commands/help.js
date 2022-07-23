44d772f7fcba46214ccb759133d9de72db3e7814
Extrymes <simon.abras@gmail.com>
2022-07-23 15:48:05 +0200

    Improved Help command

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,12 @@
 module.exports.run = async (client, message, args, settings, queue, lang) => {
     const { guild, channel } = message
-    const command = client.commands.filter(item => item.data.name !== "help").get(args[0])
+    const cmd = args[0]
+    const command = client.commands.filter(item => item.data.name !== "help").get(cmd)
 
-    if(command) client.sendHelpMessage(channel, lang, command)
-    else {
+    if(command) {
+        if(client.cooldown("help" + command.data.name + guild.id, 8000)) return client.sendError(channel, `${lang.ERROR_ACTION_TOO_FAST}`)
+        client.sendHelpMessage(channel, lang, command)
+    } else {
         if(client.cooldown("help" + guild.id, 10000)) return client.sendError(channel, `${lang.ERROR_ACTION_TOO_FAST}`)
         client.sendHelpMessage(channel, lang)
     }
