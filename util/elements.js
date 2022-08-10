1c2d39c3e245776c92b600684cb9e1fe8b908113
Extrymes <simon.abras@gmail.com>
2022-08-10 13:06:37 +0200

    Optimized Code

--- util/elements.js
+++ util/elements.js
@@ -1,11 +1,8 @@
 module.exports = {
-    // BANNER
     BANNER_FLOPY: "https://cdn.discordapp.com/attachments/909213855106891844/939203625090179193/flopy_banner.png",
     BANNER_PRIMARY: "https://cdn.discordapp.com/attachments/909213855106891844/909213955044573214/banner.png",
     BANNER_SECONDARY: "https://cdn.discordapp.com/attachments/909213855106891844/926075051650973746/banner2.png",
-    // ICON
     ICON_FLOPY: "https://cdn.discordapp.com/attachments/909213855106891844/924407201857142814/icon.png",
-    // EMOJI
     EMOJI_PLAY: "936610283143708702",
     EMOJI_PAUSE: "936607695744688128",
     EMOJI_STOP: "936608198272614520",
@@ -16,15 +13,12 @@ module.exports = {
     EMOJI_LANG_FR: "🇫🇷",
     EMOJI_LIKE: "❤️",
     EMOJI_UPDATE: "🎉",
-    // SYMBOL
     SYMBOL_LINE: "▬",
     SYMBOL_CIRCLE: "⬤",
-    // COLOR
     COLOR_FLOPY: "#3d81ee",
     COLOR_WHITE: "#ffffff",
     COLOR_GREY: "#6d6a6a",
     COLOR_PINK: "#f34e6d",
-    // INVITE
     INVITE_FLOPY: "https://discord.com/api/oauth2/authorize?client_id=909205146863566858&permissions=3172352&scope=bot%20applications.commands",
     INVITE_FLOPY2: "https://discord.com/api/oauth2/authorize?client_id=909533411247546368&permissions=3172352&scope=bot%20applications.commands",
     INVITE_FLOPY3: "https://discord.com/api/oauth2/authorize?client_id=909533975809232926&permissions=3172352&scope=bot%20applications.commands",
