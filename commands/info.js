303d94e2a70a4a798cf1f0da362f697e4c6ffa37
Extrymes <simon.abras@gmail.com>
2022-12-04 17:24:58 +0100

    Improved Info command

--- commands/info.js
+++ commands/info.js
@@ -3,15 +3,15 @@ const elements = require("../util/elements")
 const languages = require("../util/languages")
 
 module.exports.run = async (client, interaction, settings, queue, lang) => {
-    const guild = interaction.guild
+    const member = interaction.member
     const song = queue?.songs[0]
 
     if(!song) return client.replyError(interaction, false, `${lang.ERROR_SONG_NO_PLAYING}`)
-    if(client.cooldown("info" + guild.id, 8000)) return client.replyError(interaction, false, `${lang.ERROR_ACTION_TOO_FAST}`)
+    if(client.cooldown("info" + member.id, 2000)) return client.replyError(interaction, false, `${lang.ERROR_ACTION_TOO_FAST}`)
     const bar = client.createBar(queue)
     const infoEmbed = new EmbedBuilder().setAuthor({ name: `${song.name}`, url: song.url, iconURL: elements.ICON_FLOPY }).setThumbnail(song.thumbnail || elements.BANNER_SECONDARY).addFields({ name: `**${lang.MESSAGE_SONG_AUTHOR}**`, value: `${song.uploader.name}`, inline: true }, { name: `**${lang.MESSAGE_SONG_VIEWS}**`, value: `${song.views.toString().replace(/(.)(?=(\d{3})+$)/g,"$1,")}`, inline: true }, { name: `**${lang.MESSAGE_SONG_LIKES}**`, value: `${song.likes.toString().replace(/(.)(?=(\d{3})+$)/g,"$1,")}`, inline: true }, { name: `**${lang.MESSAGE_SONG_DURATION}**`, value: `${bar}` }).setColor(elements.COLOR_FLOPY)
-    interaction.reply({ embeds: [infoEmbed] }).catch(error => {})
-    setTimeout(() => interaction.deleteReply().catch(error => {}), 8000)
+    interaction.reply({ embeds: [infoEmbed], ephemeral: true }).catch(error => {})
+    setTimeout(() => interaction.deleteReply().catch(error => {}), 60000)
 }
 module.exports.data = {
     name: "info",
