fded631981da2af7eb2c583c391aedc610aee039
Extrymes <simon.abras@gmail.com>
2023-01-08 19:54:19 +0100

    Added Song subcommand to remove command

--- commands/remove.js
+++ commands/remove.js
@@ -2,15 +2,22 @@ const languages = require("../util/languages")
 
 module.exports.run = async (client, interaction, settings, queue, lang) => {
     const { guild, member, options } = interaction
-    const position = options.getInteger("position")
-    const song = queue?.songs[position]
+    const subcommand = options.getSubcommand()
 
-    if(!song) return client.sendErrorNotification(interaction, `${lang.ERROR_SONG_INVALID_POSITION}`)
-    if(!client.checkVoice(guild, member)) return client.sendErrorNotification(interaction, `${lang.ERROR_USER_MUST_JOIN_VOICE_2}`)
-    if(!client.manageCooldown("remove", guild.id, 2000)) return client.sendErrorNotification(interaction, `${lang.ERROR_ACTION_NOT_POSSIBLE}`)
-    queue.songs.splice(position, 1)
-    client.editDashboard(guild, queue, lang)
-    client.sendAdvancedNotification(interaction, `${lang.MESSAGE_QUEUE_SONG_REMOVED} (#${position})`, song.name, song.thumbnail)
+    switch(subcommand) {
+        case "song":
+            const position = options.getInteger("position")
+            const song = queue?.songs[position]
+            if(!song) return client.sendErrorNotification(interaction, `${lang.ERROR_SONG_INVALID_POSITION}`)
+            if(!client.checkVoice(guild, member)) return client.sendErrorNotification(interaction, `${lang.ERROR_USER_MUST_JOIN_VOICE_2}`)
+            if(!client.manageCooldown("remove", guild.id, 2000)) return client.sendErrorNotification(interaction, `${lang.ERROR_ACTION_NOT_POSSIBLE}`)
+            queue.songs.splice(position, 1)
+            client.editDashboard(guild, queue, lang)
+            client.sendAdvancedNotification(interaction, `${lang.MESSAGE_QUEUE_SONG_REMOVED} (#${position})`, song.name, song.thumbnail)
+            break
+        default:
+            client.sendErrorNotification(interaction, `${lang.ERROR_UNKNOWN}`)
+    }
 }
 module.exports.data = {
     name: "remove",
@@ -18,11 +25,19 @@ module.exports.data = {
     description_localizations: { "fr": languages["fr"].COMMAND_REMOVE_DESCRIPTION },
     options: [
         {
-            name: "position",
-            description: languages["en"].COMMAND_REMOVE_OPTION,
-            description_localizations: { "fr": languages["fr"].COMMAND_REMOVE_OPTION },
-            type: 4,
-            required: true,
+            name: "song",
+            description: languages["en"].COMMAND_REMOVE_SONG_DESCRIPTION,
+            description_localizations: { "fr": languages["fr"].COMMAND_REMOVE_SONG_DESCRIPTION },
+            type: 1,
+            options: [
+                {
+                    name: "position",
+                    description: languages["en"].COMMAND_REMOVE_SONG_OPTION,
+                    description_localizations: { "fr": languages["fr"].COMMAND_REMOVE_SONG_OPTION },
+                    type: 4,
+                    required: true,
+                },
+            ],
         },
     ],
     dm_permission: false,
