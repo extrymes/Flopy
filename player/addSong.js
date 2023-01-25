fd2c7f60e2e4c56ca416b4bc2a325ac006724fba
Extrymes <simon.abras@gmail.com>
2023-01-25 14:07:57 +0100

    Fixed Small issue

--- player/addSong.js
+++ player/addSong.js
@@ -7,7 +7,7 @@ module.exports = async (client, queue, song) => {
     const lang = languages[settings.flopy1.language]
     
     client.queries.set(song.member.id, song.url)
-    queue.songs.splice(1, queue.songs.length - 1 - client.config.QUEUE_MAX_LENGTH)
+    if(queue.songs.length - 1 > client.config.QUEUE_MAX_LENGTH) queue.songs.splice(queue.songs.indexOf(song) > 1 ? 1 : client.config.QUEUE_MAX_LENGTH + 1, queue.songs.length - 1 - client.config.QUEUE_MAX_LENGTH)
     if(queue.songs[0] !== song) {
         client.editDashboard(guild, queue, lang)
         client.sendAdvancedNotification(destination, `${lang.MESSAGE_QUEUE_SONG_ADDED} (#${queue.songs.indexOf(song)})`, song.name, song.thumbnail, true)
