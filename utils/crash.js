9272eb9012ed4c88299f0b669ea3cbb4bb20624f
Extrymes <simon.abras@gmail.com>
2023-01-26 21:06:08 +0100

    Optimized Code

--- utils/crash.js
+++ utils/crash.js
@@ -1,5 +1,5 @@
 module.exports = client => {
     process.on("unhandledRejection", (reason, p) => console.warn(reason, p))
-    process.on("uncaughtException", (err, origin) => console.warn(err, origin)) 
-    process.on("uncaughtExceptionMonitor", (err, origin) => console.warn(err, origin))
+    process.on("uncaughtException", (error, origin) => console.warn(error, origin)) 
+    process.on("uncaughtExceptionMonitor", (error, origin) => console.warn(error, origin))
 }
