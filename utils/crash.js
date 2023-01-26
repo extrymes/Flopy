1f59e649851871563a7549b8ea050d24680ab655
Extrymes <simon.abras@gmail.com>
2023-01-26 13:35:02 +0100

    Fixed Deprecations for node.js

--- utils/crash.js
+++ utils/crash.js
@@ -2,5 +2,4 @@ module.exports = client => {
     process.on('unhandledRejection', (reason, p) => console.warn(reason, p))
     process.on("uncaughtException", (err, origin) => console.warn(err, origin)) 
     process.on('uncaughtExceptionMonitor', (err, origin) => console.warn(err, origin))
-    process.on('multipleResolves', (type, promise, reason) => {})
 }
