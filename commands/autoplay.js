46164fd09db9147b5f86b56db7f862110ef4a720
extrymes <simon.abras@gmail.com>
2023-12-01 22:17:28 +0100

    style: adjust code indentation in autoplay.js

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -9,10 +9,10 @@ module.exports = {
     .setDMPermission(false),
   run: async (client, interaction, settings, queue, lang) => {
     const { guild, member } = interaction;
-    
-    if(!queue?.songs[0]) return client.sendErrorNotification(interaction, `${lang.ERROR_SONG_NO_PLAYING}`);
-    if(!client.checkVoice(guild, member)) return client.sendErrorNotification(interaction, `${lang.ERROR_USER_MUST_JOIN_VOICE_2}`);
-    if(!client.manageCooldown("autoplay", guild.id, 2000)) return client.sendErrorNotification(interaction, `${lang.ERROR_ACTION_NOT_POSSIBLE}`);
+
+    if (!queue?.songs[0]) return client.sendErrorNotification(interaction, `${lang.ERROR_SONG_NO_PLAYING}`);
+    if (!client.checkVoice(guild, member)) return client.sendErrorNotification(interaction, `${lang.ERROR_USER_MUST_JOIN_VOICE_2}`);
+    if (!client.manageCooldown("autoplay", guild.id, 2000)) return client.sendErrorNotification(interaction, `${lang.ERROR_ACTION_NOT_POSSIBLE}`);
     const autoplay = client.distube.toggleAutoplay(queue);
     client.editDashboard(guild, queue, lang);
     client.sendNotification(interaction, `${autoplay ? lang.MESSAGE_AUTOPLAY_ENABLED : lang.MESSAGE_AUTOPLAY_DISABLED}`);
