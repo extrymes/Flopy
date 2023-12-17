91a3b2fa6284111b6eb290411d9a625c6380d0b4
extrymes <simon.abras@gmail.com>
2023-12-18 00:10:21 +0100

    chore: remove unnecessary searchNoResult.js file

--- player/searchNoResult.js
+++
@@ -1,9 +0,0 @@
-const languages = require("../utils/languages");
-
-module.exports = async (client, message, query) => {
-  const { guild, channel } = message;
-  const guildData = await client.getGuildData(guild);
-  const lang = languages[guildData.language];
-
-  client.sendErrorNotification(channel, `${lang.ERROR_RESULT_NO_FOUND}`);
-}
