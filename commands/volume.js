03fda1424eacf1450db745aa174be42af10bda49
extrymes <simon.abras@gmail.com>
2023-12-18 16:39:42 +0100

    style(volume): replace `volume` variable with `percentage` for better naming conventions

--- commands/volume.js
+++ commands/volume.js
@@ -16,20 +16,20 @@ module.exports = {
     ),
   run: async (client, interaction, guildData, queue, lang) => {
     const { guild, member, options } = interaction;
-    const volume = options.getInteger("percentage");
+    const percentage = options.getInteger("percentage");
 
     if (!queue?.songs[0]) return client.sendErrorNotification(interaction, `${lang.ERROR_SONG_NO_PLAYING}`);
     if (!client.checkMemberIsInMyVoiceChannel(guild, member)) return client.sendErrorNotification(interaction, `${lang.ERROR_MEMBER_MUST_JOIN_MY_VOICE_CHANNEL}`);
-    if (volume < 1 || volume > 100) return client.sendErrorNotification(interaction, `${lang.ERROR_VOLUME_INVALID_PERCENTAGE}`);
-    if (volume === queue.volume) return client.sendErrorNotification(interaction, `${lang.ERROR_VOLUME_ALREADY_SET.replace("$volume", `\`${volume}%\``)}`);
+    if (percentage < 1 || percentage > 100) return client.sendErrorNotification(interaction, `${lang.ERROR_VOLUME_INVALID_PERCENTAGE}`);
+    if (percentage === queue.volume) return client.sendErrorNotification(interaction, `${lang.ERROR_VOLUME_ALREADY_SET.replace("$percentage", `\`${percentage}%\``)}`);
     if (!client.handleCooldown("volumeCommand", guild.id, 2000)) return client.sendErrorNotification(interaction, `${lang.ERROR_ACTION_NOT_POSSIBLE}`);
     await interaction.deferReply().catch((error) => { });
     try {
       // Set volume percentage
-      await client.distube.setVolume(queue, volume);
+      await client.distube.setVolume(queue, percentage);
       // Update dashboard message and send notification
       client.updateDashboardMessage(guild, queue, lang);
-      client.sendNotification(interaction, `${lang.MESSAGE_VOLUME_SET.replace("$volume", `\`${volume}%\``)}`, { editReply: true });
+      client.sendNotification(interaction, `${lang.MESSAGE_VOLUME_SET.replace("$percentage", `\`${percentage}%\``)}`, { editReply: true });
     } catch (error) {
       const errorMessage = client.getErrorMessage(error.message, lang);
       client.sendErrorNotification(interaction, `${errorMessage}`, { editReply: true });
