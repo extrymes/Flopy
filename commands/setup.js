4ba8c11d4a4aed11296bdd0f5dd08d6e67209b7c
extrymes <simon.abras@gmail.com>
2023-12-26 20:52:15 +0100

    feat(setup): send notification when dashboard is installed and use selected language

--- commands/setup.js
+++ commands/setup.js
@@ -19,19 +19,20 @@ module.exports = {
   run: async (client, interaction, guildData, queue, lang) => {
     const { guild, channel, member, options } = interaction;
     const language = options.getString("language");
+    const selectedLang = languages[language];
 
-    if (!client.checkMemberIsManager(member)) return client.sendErrorNotification(interaction, `${lang.ERROR_MEMBER_MUST_BE_MANAGER}`);
-    if (!client.checkMessageIsSendable(guild, channel)) return client.sendErrorNotification(interaction, `${lang.ERROR_DASHBOARD_UNABLE_SETUP}`);
-    if (!client.handleCooldown("setupCommand", guild.id, 4000)) return client.sendErrorNotification(interaction, `${lang.ERROR_ACTION_NOT_POSSIBLE}`);
+    if (!client.checkMemberIsManager(member)) return client.sendErrorNotification(interaction, `${selectedLang.ERROR_MEMBER_MUST_BE_MANAGER}`);
+    if (!client.checkMessageIsSendable(guild, channel)) return client.sendErrorNotification(interaction, `${selectedLang.ERROR_DASHBOARD_UNABLE_SETUP}`);
+    if (!client.handleCooldown("setupCommand", guild.id, 4000)) return client.sendErrorNotification(interaction, `${selectedLang.ERROR_ACTION_NOT_POSSIBLE}`);
     await interaction.deferReply({ ephemeral: true }).catch((error) => { });
     try {
       // Update language in database
       if (language !== guildData.language) await client.updateGuildData(guild, { language: language });
       // Send new dashboard message and then delete reply
-      await client.sendDashboardMessage(guild, channel, queue, languages[language]);
-      interaction.deleteReply().catch((error) => { });
+      await client.sendDashboardMessage(guild, channel, queue, selectedLang);
+      client.sendNotification(interaction, `${selectedLang.MESSAGE_DASHBOARD_INSTALLED}`, { editReply: true });
     } catch (error) {
-      const errorMessage = client.getErrorMessage(error.message, lang);
+      const errorMessage = client.getErrorMessage(error.message, selectedLang);
       client.sendErrorNotification(interaction, `${errorMessage}`, { editReply: true });
       client.leaveVoiceChannel(guild);
     }
