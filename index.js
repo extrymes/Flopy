a44176b90625279b28990a357cad7d8d3360f181
extrymes <simon.abras@gmail.com>
2024-06-15 15:04:04 +0200

    feat: add Spotify distube plugin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
 const { DisTube } = require("distube");
 const { YouTubePlugin } = require("@distube/youtube");
+const { SpotifyPlugin } = require("@distube/spotify");
 const mongoose = require("mongoose");
 const fs = require("fs");
 const colors = require("colors");
@@ -23,7 +24,7 @@ client.youtubePlugin = new YouTubePlugin({
 
 // Create DisTube instance
 client.distube = new DisTube(client, {
-	plugins: [client.youtubePlugin],
+	plugins: [client.youtubePlugin, new SpotifyPlugin()],
 	nsfw: config.DISTUBE_NSFW,
 	savePreviousSongs: config.DISTUBE_SAVE_PREVIOUS_SONGS,
 	customFilters: config.DISTUBE_CUSTOM_FILTERS
