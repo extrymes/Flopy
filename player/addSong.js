a3326b4b46d99cbfca3ef958eaa18141d37aa993
extrymes <simon.abras@gmail.com>
2024-06-15 15:19:09 +0200

    chore(translation): add `MESSAGE_NOW_PLAYING` and remove `MESSAGE_SONG_PLAYING` and `MESSAGE_PLAYLIST_PLAYING` translation keys

--- player/addSong.js
+++ player/addSong.js
@@ -11,7 +11,7 @@ module.exports = async (client, queue, song) => {
 	// Remove excess songs from queue if limit is reached
 	if (songs.length - 1 > config.QUEUE_MAX_LENGTH) songs.splice(songs.indexOf(song) > 1 ? 1 : config.QUEUE_MAX_LENGTH + 1, songs.length - 1 - config.QUEUE_MAX_LENGTH);
 	// Send advanced notification and update dashboard message
-	if (songs[0] === song) return setTimeout(() => client.sendAdvancedNotification(destination, `${lang.MESSAGE_SONG_PLAYING}`, `${song.name}`, song.thumbnail, { editReply: true }), 1000);
+	if (songs[0] === song) return setTimeout(() => client.sendAdvancedNotification(destination, `${lang.MESSAGE_NOW_PLAYING}`, `${song.name}`, song.thumbnail, { editReply: true }), 1000);
 	client.updateDashboardMessage(guild, queue, lang);
 	client.sendAdvancedNotification(destination, `${lang.MESSAGE_QUEUE_SONG_ADDED} (#${songs.indexOf(song)})`, `${song.name}`, song.thumbnail, { editReply: true });
 }
