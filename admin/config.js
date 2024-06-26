656fd2d3ff5a399ddd21eb7106c9c833c23db330
extrymes <simon.abras@gmail.com>
2024-06-26 20:12:06 +0200

    refactor: use new map of distube error messages

--- admin/config.js
+++ admin/config.js
@@ -17,5 +17,43 @@ module.exports = {
 	DISTUBE_SEARCH_MAX_RESULTS: 20, // Max. number of search results
 	DISTUBE_NSFW: true, // Play age-restricted content
 	DISTUBE_SAVE_PREVIOUS_SONGS: false, // Save previous songs in queue
-	DISTUBE_CUSTOM_FILTERS: { "8d": "apulsator=hz=0.08", "purebass": "bass=g=20,dynaudnorm=f=200,asubboost", "subboost": "asubboost", "fast": "atempo=1.3", "vibrato": "vibrato=f=6.5", "pulsator": "apulsator=hz=1" } // Custom ffmpeg filters
+	DISTUBE_CUSTOM_FILTERS: { "8d": "apulsator=hz=0.08", "purebass": "bass=g=20,dynaudnorm=f=200,asubboost", "subboost": "asubboost", "fast": "atempo=1.3", "vibrato": "vibrato=f=6.5", "pulsator": "apulsator=hz=1" }, // Custom ffmpeg filters
+	DISTUBE_ERROR_MAPPING: [ // Map of distube error messages
+		{
+			keywords: ["I do not have permission to join this voice channel", "Cannot connect to the voice channel", "The voice channel is full"],
+			message: "ERROR_VOICE_CHANNEL_UNABLE_JOIN"
+		},
+		{
+			keywords: ["No result found", "Cannot resolve undefined to a Song", "search string is mandatory"],
+			message: "ERROR_RESULT_NO_FOUND"
+		},
+		{
+			keywords: ["Video unavailable", "This video is unavailable", "Premiere will begin shortly"],
+			message: "ERROR_VIDEO_UNAVAILABLE"
+		},
+		{
+			keywords: ["Sign in to confirm your age", "Sorry, this content is age-restricted", "This video is only available to Music Premium members"],
+			message: "ERROR_VIDEO_RESTRICTED"
+		},
+		{
+			keywords: ["Sign in to confirm you’re not a bot"],
+			message: "ERROR_COOKIES_INVALID"
+		},
+		{
+			keywords: ["Unsupported URL", "This url is not supported"],
+			message: "ERROR_URL_UNSUPPORTED"
+		},
+		{
+			keywords: ["Invalid track ID"],
+			message: "ERROR_SONG_INVALID_ID"
+		},
+		{
+			keywords: ["Invalid URL"],
+			message: "ERROR_URL_INVALID"
+		},
+		{
+			keywords: ["Unknown Playlist"],
+			message: "ERROR_PLAYLIST_UNKNOWN"
+		}
+	]
 }
--- utils/functions.js
+++ utils/functions.js
@@ -188,15 +188,11 @@ module.exports = (client) => {
 
 	// Get error message
 	client.getErrorMessage = (error, lang) => {
-		if (error.includes("I do not have permission to join this voice channel") || error.includes("Cannot connect to the voice channel") || error.includes("The voice channel is full")) return `${lang.ERROR_VOICE_CHANNEL_UNABLE_JOIN}`;
-		if (error.includes("No result found") || error.includes("Cannot resolve undefined to a Song") || error.includes("search string is mandatory")) return `${lang.ERROR_RESULT_NO_FOUND}`;
-		if (error.includes("Video unavailable") || error.includes("This video is unavailable") || error.includes("Premiere will begin shortly")) return `${lang.ERROR_VIDEO_UNAVAILABLE}`;
-		if (error.includes("Sign in to confirm your age") || error.includes("Sorry, this content is age-restricted") || error.includes("This video is only available to Music Premium members")) return `${lang.ERROR_VIDEO_RESTRICTED}`;
-		if (error.includes("Sign in to confirm you’re not a bot")) return `${lang.ERROR_COOKIES_INVALID}`;
-		if (error.includes("Unsupported URL") || error.includes("This url is not supported")) return `${lang.ERROR_URL_UNSUPPORTED}`;
-		if (error.includes("Invalid track ID")) return `${lang.ERROR_SONG_INVALID_ID}`;
-		if (error.includes("Invalid URL")) return `${lang.ERROR_URL_INVALID}`;
-		if (error.includes("Unknown Playlist")) return `${lang.ERROR_PLAYLIST_UNKNOWN}`;
+		for (const { keywords, message } of config.DISTUBE_ERROR_MAPPING)
+		{
+			if (keywords.some((keyword) => error.includes(keyword)))
+				return `${lang[message]}`;
+		}
 		console.error(error);
 		return `${lang.ERROR_UNKNOWN}`;
 	}
